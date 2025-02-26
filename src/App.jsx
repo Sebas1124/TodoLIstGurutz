308584d44a043aefc032368bd52fac1084683126
Sebastian Rosero Lopez <srosero87@misena.edu.co>
2025-02-26 11:46:24 +0100

    Linea para gurutz

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>To do List</h1>
+      <h1>To do List hola soy sebas desde mi ordenador</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
